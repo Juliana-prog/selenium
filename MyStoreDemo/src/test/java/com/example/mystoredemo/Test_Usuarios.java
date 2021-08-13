8ca7c082fa75481736046778a0965816711def3b
Juliana-prog <salajuliana27@gmail.com>
2021-08-12 21:12:32 -0300

    Modificacion Test_Usuarios Modificacion data.json, agrego campos para veficar Test_Usuarios

--- MyStoreDemo/src/test/java/com/example/mystoredemo/Test_Usuarios.java
+++ MyStoreDemo/src/test/java/com/example/mystoredemo/Test_Usuarios.java
@@ -9,8 +9,10 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.*;
 import org.testng.asserts.SoftAssert;
+
 
 import java.io.File;
 import java.io.FileReader;
@@ -38,22 +40,31 @@ public class Test_Usuarios {
     }
 
     @Test(dataProvider = "data")
-    public void login(String mail,String pass) {
+    public void login(String mail,String pass, String name) {
+//      Ingreso pagina logueo/Login
         WebElement webElement = driver.findElement(By.cssSelector(".login"));
         softAssert.assertEquals(webElement.getAttribute("innerText"), "Sign in");
         webElement.click();
         driver.findElement(By.cssSelector("#email")).sendKeys(mail);
         driver.findElement(By.cssSelector("#passwd")).sendKeys(pass);
         driver.findElement(By.cssSelector("#passwd")).sendKeys(Keys.ENTER);
+
+
+//      Verificaciones de logueo Satisfactorio
+        try {
+            webElement = driver.findElement(By.cssSelector(".alert-danger li"));
+            Assert.assertFalse(webElement.isDisplayed(), "Error en autentificacion\n Pag Error:" + webElement.getText());
+        } catch (Exception e){
+            System.out.println("No hay problemas de autentificacion,\nno such element: Unable to locate element: {\"method\":\"css selector\",\"selector\":\".alert-danger li\"} ");
+
+        }
+
         webElement = driver.findElement(By.cssSelector(".logout"));
-        softAssert.assertEquals(webElement.getAttribute("innerText"), "Sign out");
         softAssert.assertTrue(webElement.isDisplayed(),"Usuario no logueado");
+        softAssert.assertEquals(driver.findElement(By.cssSelector(".account")).getText(),name,"Usuario Logueado, pero no es el correspondiente");
+
+//       Desloguearse/Log out
         webElement.click();
-        //esto lo agrego en el branch
-        //sigo escribiendo fuera del branch, pero hice un merge asi que veo el comentario del branch
-//        pero el no puede ver el mio MAS CAMBIOS
-        System.out.println("quiero hacer mas cambios");
-//        jnkajfbkjfbe
     }
 
     @DataProvider (name="data")
@@ -62,12 +73,14 @@ public class Test_Usuarios {
         FileReader reader = new FileReader(new File("data.json").getAbsolutePath());
         //Read JSON file
         JSONArray jsonA = (JSONArray) jsonParser.parse(reader);
-        Object[][] datos = new Object[jsonA.size()][2];
+
+        Object[][] datos = new Object[jsonA.size()][3];
         for (int i=0; i<jsonA.size();i++) {
             JSONObject json =(JSONObject)jsonA.get(i);
             JSONObject usuario = (JSONObject) json.get("usuario");
             datos[i][0]=usuario.get("mail");
             datos[i][1]=usuario.get("pass");
+            datos[i][2]=usuario.get("name");
         }
         return datos;
     }
